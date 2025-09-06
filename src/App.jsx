14f249e3d4a18d7a204ca49d30eddf073bd8af3b
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-09-06 14:53:26 +0200

    Add first part of the Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <main className="text-5xl text-violet-300 font-circular-web">
-      Welcome to Awwwards
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 }
